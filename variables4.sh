61cbfa3441834c01f881f6d290127ba1f46ad945
sivajanardhan9989@gmail.com <sivajanardhan9989@gmail.com>
2026-06-23 21:58:12 +0530

    first script

---
+++ variables4.sh
@@ -0,0 +1,7 @@
+echo "please enter the name of  name::"
+read name #here USER_NAME is variable, whatever you enter in terminal stores in this variable
+echo "You entered: $name"
+
+echo "please enter password::"
+read -s password #here USER_PASSWORD is variable, whatever you enter in terminal stores in this variable
+echo "You entered: $password"

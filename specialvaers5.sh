37be8f7d0416d6f02b836c11666c07019df6919d
sivajanardhan9989@gmail.com <sivajanardhan9989@gmail.com>
2026-06-23 22:14:10 +0530

    first script

--- specialvaers5.sh
+++ specialvaers5.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
-echo "all variables passed in command line are $@"
-echo "all variables passed in command line are $*"
-echo "first variable passed in command line is $1"
-echo "second variable passed in command line is $2"
-echo "total number of variables passed in command line are $#"
-echo "Script name: $0"
-echo "PID of current script: $$"
-echo  "current user is $USER"
-echo "Line number: $LINENO"
-echo "Exit code of previous command: $?"
 
+##### Special Vars #####
+echo "All variables passed to script: $@"
+echo "Number of variables passed: $#"
+echo "First variable: $1"
+echo "Script name: $0"
+echo "Who is running this: $USER"
+echo "Which directory: $PWD"
+echo "Home directory: $HOME"
+echo "PID of the current script: $$"
+sleep 5 &
+echo "PID of the background command running just now: $!"
+wait $!
+echo "Line number: $LINENO"
+echo "Script executed in $SECONDS seconds"
+echo "Random number: $RANDOM"
+echo "Exit code of previous command: $?"

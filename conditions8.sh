b61b47fff6097f298f2fa66190c2822e9ca45dbc
sivajanardhan9989@gmail.com <sivajanardhan9989@gmail.com>
2026-06-23 22:24:16 +0530

    first script

---
+++ conditions8.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# gt - greater than
+# lt - less than
+# eq - equal
+# ne - not equal
+# ge - greater than or equal
+# le - less than or equal
+
+
+number=$1;
+
+if [$number -gt 5]; then
+    echo "The number is greater than 5"
+elif [$number -lt 5]; then
+    echo "The number is less than 5"
+else
+    echo "The number is equal to 5"
+fi
+
+
+
+

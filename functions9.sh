aedef9f226c352d5d63782be754d70eabc4984e7
sivajanardhan9989@gmail.com <sivajanardhan9989@gmail.com>
2026-06-23 22:37:51 +0530

    first script

---
+++ functions9.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+ USERID=$(id -u)
+
+if [ $USERID -ne 0 ]; then
+    echo "please run this script as root user"
+    exit 1
+ fi  
+
+ # first arg -> what are you trying to install
+# second arg -> exit code
+validate() {
+    if [ $2 -ne 0 ]; then
+        echo "Error occurred while installing $1"
+        exit 1
+    else
+        echo "$1 installed successfully"    
+    fi
+}
+
+# echo "I am continuing..."
+dnf list installed mysql 

5454eb7f67ca8eafffd360d702d9f69d1875a82c
sivajanardhan9989@gmail.com <sivajanardhan9989@gmail.com>
2026-06-23 22:05:34 +0530

    first script

--- variables5.sh
+++ variables5.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 START_TIME=$(date +%s)
+echo "Script started at $(date)"
 
 # Your script logic here
 
 END_TIME=$(date +%s)
+echo "Script ended at $(date)"
 DURATION=$((END_TIME - START_TIME))
+
 
 echo "Script completed in ${DURATION} seconds"

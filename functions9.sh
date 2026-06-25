b47739070b4473656bdb83e48bdda3fe4d58e972
sivajanardhan9989@gmail.com <sivajanardhan9989@gmail.com>
2026-06-25 09:25:01 +0530

    first script

--- functions9.sh
+++ functions9.sh
@@ -6,15 +6,29 @@ if [ $USERID -ne 0 ]; then
     exit 1
  fi  
 
-dnf install mysql -y 
+
+ #first arg -> what are you trying to install
+# second arg -> exit code
+
+
+ validate() {
+    if [ $2 -ne 0 ]; then
+        echo "$1  isntallaion failed to install"
+    else
+        echo "$1 installed successfully"
+    fi
+ }
 
 dnf list installed mysql
 
 if [ $? -eq 0 ]; then
-    echo "mysql is installed"
+    echo "mysql is  already installed"
 else
     echo "mysql is not installed"
+    validate "mysql" $?
 fi
+
+
 
 
 

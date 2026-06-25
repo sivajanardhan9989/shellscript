849f2d07de39af135a7ffe1f9bc82a051484f02b
sivajanardhan9989@gmail.com <sivajanardhan9989@gmail.com>
2026-06-25 09:11:03 +0530

    first script

--- functions9.sh
+++ functions9.sh
@@ -6,25 +6,7 @@ if [ $USERID -ne 0 ]; then
     exit 1
  fi  
 
- # first arg -> what are you trying to install
-# second arg -> exit code
-validate() {
-    if [ $2 -ne 0 ]; then
-        echo "Error occurred while installing $1"
-        exit 1
-    else
-        echo "$1 installed successfully"    
-    fi
-}
+dnf install mysql -y 
 
-# echo "I am continuing..."
-dnf list installed mysql 
 
-if [ $? -eq 0 ]; then
-    echo "MySQL is already installed ... SKIPPING"
-else
-    echo "Installing MySQL"
-    dnf install mysql -y
-    validate MySQL $?
- fi
-    
+
